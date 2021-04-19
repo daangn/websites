b64744ba5931cf2d71ac18069957f5f6df0a88d4
Hyeseong Kim <tim@daangn.com>
2021-04-19 14:30:11 +0900

    PR을 한 번 날려 봅니다

--- team.daangn.com/src/pages/jobs/index.tsx
+++ team.daangn.com/src/pages/jobs/index.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
 import type { PageProps } from 'gatsby';
 import { graphql, Link } from 'gatsby';
+import { styled } from 'gatsby-theme-stitches/src/stitches.config';
 
 type JobsPageProps = PageProps<GatsbyTypes.JobsPageQuery>;
+
+const Title = styled('h1', {
+  color: '$carrot500',
+});
 
 const JobsPage: React.FC<JobsPageProps> = ({
   data,
 }) => {
   return (
     <div>
-      <h1>공고 보기</h1>
+      <Title>공고 보기</Title>
       <ul>
         {data.allJobPost.nodes.map(node => (
           <li key={node.id}>
