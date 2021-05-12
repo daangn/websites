d44a8a02ef484c39c3f840ecced6a26259d1278e
juyeong1260 <juyeong1260@gmail.com>
2021-05-13 01:19:22 +0900

    feat: add viewport meta

--- mbti.daangn.com/src/components/Layout.tsx
+++ mbti.daangn.com/src/components/Layout.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { colors } from '@daangn/design-token'
 import { DaangnThemeProvider } from '@daangn/emotion-react-theme'
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
+import { Helmet } from 'react-helmet-async'
 
 import { useSiteMeta } from '@src/hooks/useSiteMeta'
 
@@ -20,6 +21,12 @@ const Layout: React.FC = ({ children }) => {
         canonical={data.site?.siteMetadata.siteUrl}
         twitter={{ cardType: 'summary_large_image', handle: '@daangnteam' }}
       />
+      <Helmet>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1, user-scalable=no"
+        />
+      </Helmet>
 
       {children}
     </DaangnThemeProvider>
