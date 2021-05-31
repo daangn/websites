527004727898b75a01f8963241c64a8fc301aed9
Hyeseong Kim <tim@daangn.com>
2021-05-31 21:47:59 +0900

    team.daangn.com: 팀생활 페이지 스타일 구현

--- team.daangn.com/src/pages/life.tsx
+++ team.daangn.com/src/pages/life.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
+import { rem } from 'polished';
 import type { PageProps } from 'gatsby';
 import { graphql } from 'gatsby';
+import { styled } from 'gatsby-theme-stitches/src/stitches.config';
 import { required } from '@cometjs/core';
+import { mapAbstractTypeWithDefault } from '@cometjs/graphql-utils';
 
-import PageTitle from '~/components/PageTitle';
+import _PageTitle from '~/components/PageTitle';
+import PrismicTeamContentsDataLifeBodyLifeContent from '~/components/PrismicTeamContentsDataLifeBodyLifeContent';
 
 type LifePageProps = PageProps<GatsbyTypes.LifePageQuery, GatsbyTypes.SitePageContext>;
 
@@ -16,21 +20,54 @@ export const query = graphql`
         life_page_title {
           text
         }
+        life_body {
+          __typename
+          ...PrismicTeamContentsDataLifeBodyLifeContent_data
+        }
       }
     }
   }
 `;
+
+const Container = styled('div', {
+  maxWidth: rem(760),
+  margin: '0 auto',
+});
+
+const PageTitle = styled(_PageTitle, {
+  marginBottom: rem(56),
+
+  '@sm': {
+    marginBottom: rem(120),
+  },
+});
+
+const Content = styled('div', {
+  display: 'grid',
+  gap: rem(64),
+});
 
 const LifePage: React.FC<LifePageProps> = ({
   data,
 }) => {
   required(data.prismicTeamContents);
   return (
-    <>
+    <Container>
       <PageTitle size={{ '@sm': 'sm' }}>
         {data.prismicTeamContents.data.life_page_title?.text}
       </PageTitle>
-    </>
+      <Content>
+        {data.prismicTeamContents.data.life_body.map((data, i) => mapAbstractTypeWithDefault(data, {
+          PrismicTeamContentsDataLifeBodyLifeContent: data => (
+            <PrismicTeamContentsDataLifeBodyLifeContent
+              key={i}
+              data={data}
+            />
+          ),
+          _: null,
+        }))}
+      </Content>
+    </Container>
   );
 };
 
