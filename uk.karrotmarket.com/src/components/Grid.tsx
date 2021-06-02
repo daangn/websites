6541fc091c808f1fb2551f363fea30a249466d7d
Lewis <lewis@daangn.com>
2021-06-02 23:46:09 +0900

    fix: karrot logo in download section

--- uk.karrotmarket.com/src/components/Grid.tsx
+++ uk.karrotmarket.com/src/components/Grid.tsx
@@ -10,10 +10,14 @@ interface TGrid {
     rowGap?: string | number | MediaTypeMap<string | number>;
     marginTop?: string | number | MediaTypeMap<string | number>;
     marginBottom?: string | number | MediaTypeMap<string | number>;
+    width?: string | number | MediaTypeMap<string | number>;
+    height?: string | number | MediaTypeMap<string | number>;
+    padding?: string | number | MediaTypeMap<string | number>;
 }
 
 const SGrid = styled("div", {
     display: "grid",
+    boxSizing: "border-box",
 });
 
 export const Grid: React.FC<TGrid> = (props) => {
--- uk.karrotmarket.com/src/components/phoneMockup/phoneMockupVerify/Verify.tsx
+++ uk.karrotmarket.com/src/components/phoneMockup/phoneMockupVerify/Verify.tsx
@@ -2,12 +2,29 @@ import React from "react";
 import { styled } from "gatsby-theme-stitches/src/stitches.config";
 import { motion } from "framer-motion";
 import { em } from "polished";
+import { Space } from "@src/components/Space";
 
 const Pin = () => (
     <svg width={em(32)} viewBox="0 0 26 33" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path
             d="M25.3503 13.6301C25.3503 17.4857 23.2377 22.0803 17.9564 25.2491C13.9057 27.6795 12.675 32.643 12.675 32.643C12.675 32.643 11.4443 27.6795 7.39365 25.2491C2.1123 22.0803 -0.000244141 17.4857 -0.000244141 13.6301C-0.000244141 6.6298 5.67466 0.954895 12.675 0.954895C19.6753 0.954895 25.3503 6.6298 25.3503 13.6301Z"
             fill="#FF7E36"
+        />
+    </svg>
+);
+
+const Check = () => (
+    <svg width={em(18)} height={em(14)} viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M16.5523 0.995684C16.8488 1.24278 16.8888 1.68348 16.6417 1.98L7.90567 12.4633C7.78701 12.6057 7.61664 12.6951 7.43204 12.7119C7.24745 12.7287 7.06375 12.6714 6.92135 12.5528L1.67971 8.18473C1.38319 7.93763 1.34313 7.49694 1.59023 7.20042C1.83733 6.9039 2.27802 6.86383 2.57454 7.11093L7.27929 11.0316L15.5679 1.08517C15.815 0.788646 16.2557 0.748583 16.5523 0.995684Z"
+            fill="white"
+            stroke="white"
+            stroke-width="0.873607"
+            stroke-miterlimit="10"
+            stroke-linecap="round"
+            stroke-linejoin="round"
         />
     </svg>
 );
@@ -37,6 +54,9 @@ const LocationText = styled(motion.div, {
     textAlign: "left",
 });
 const Button = styled(motion.button, {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     background: "$carrot500",
     color: "white",
     width: "100%",
@@ -119,6 +139,8 @@ const Verify: React.FC<VerifyProps> = ({ mapImage, buttonText, location }) => {
                         duration: 0.5,
                     }}
                 >
+                    <Check></Check>
+                    <Space w={em(6)}></Space>
                     {buttonText}
                 </Button>
             </BottomSection>
