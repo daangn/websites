9de9eb1e16c61a41c3250b0bbabe76a7609fc2b6
Hyeseong Kim <tim@daangn.com>
2021-06-04 15:19:13 +0900

    index for every content types

---
+++ _packages/@karrotmarket/prismic-config/schema/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'mbti-test-result': {},
+  mbti_intro: {},
+  mbti_test_question: {},
+  ads_intro: {},
+  global_contents: {},
+  faq: require('./faq.json'),
+  member_profile: require('./pay_contents.json'),
+  pay_contents: require('./pay_contents.json'),
+  site_navigation: require('./site_navigation.json'),
+  team_contents: require('./team_contents.json'),
+  terms_and_conditions: require('./terms_and_conditions.json'),
+};
