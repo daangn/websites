77568d197bae4adee4c6948b37356b4c66b01866
chinchilla <76946953+lewiiiis@users.noreply.github.com>
2021-06-18 22:27:51 +0900

    fix

--- _packages/@karrotmarket/gatsby-theme-global-website/src/pages/hot_articles.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const HotArticles: React.FC = () => {
-  return <div></div>;
-};
-
-export default HotArticles;
--- _packages/@karrotmarket/gatsby-theme-global-website/src/pages/policy/terms.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const Terms: React.FC = () => {
-  return <div></div>;
-};
-
-export default Terms;
