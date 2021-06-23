ae5472feed1203839bf8e8de6e4b3f273d120535
Hyeseong Kim <tim@daangn.com>
2021-06-24 08:22:17 +0900

    chore(global): 의도치않게 벤더링된 파일들 제거

--- uk.karrotmarket.com/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.wrapRootElement = void 0;
-
-var _wrapRootElement = require("./wrapRootElement");
-
-exports.wrapRootElement = _wrapRootElement.wrapRootElement;
