0b35bf0264481a1b5b3799644ec9a76bbb6217d2
Hyeseong Kim <tim@daangn.com>
2021-06-29 17:09:04 +0900

    fix(www.daangnpay.com): Prismic Plugin Setup (#131)
    * feat: add @karrotmarket/gatsby-theme-website
    
    * feat: add @karrotmarket/gatsby-theme-prismic config
    
    chore: yarn.lock
    
    * update prismic plugin
    
    * temp: codegen error
    
    Co-authored-by: DylanJu <dylan@daangnpay.com>

--- www.daangnpay.com/src/pages/index.tsx
+++ www.daangnpay.com/src/pages/index.tsx
@@ -5,148 +5,143 @@ import {
   globalStyles,
 } from "gatsby-theme-stitches/src/stitches.config";
 import { graphql } from "gatsby";
-import { LogoJsonLd } from "gatsby-plugin-next-seo";
-import { useSiteOrigin } from '@karrotmarket/gatsby-theme-website/src/siteMetadata';
 
-import logoUrl from '../assets/logo.png';
+import JsonLd from "../components/JsonLd";
 import HeroSection from "../components/HeroSection";
 import TitleAndIllustration from "../components/TitleAndIllustration";
 import WideBanner from "../components/WideBanner";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-type IndexPageProps = PageProps<{
-  prismicPayContentsBodyHero: GatsbyTypes.PrismicPayContentsBodyHero;
-  prismicPayContentsBodyTitleAndIllustration: GatsbyTypes.PrismicPayContentsBodyTitleAndIllustration;
-  prismicPayContentsBodyTitleAndDescription: GatsbyTypes.PrismicPayContentsBodyTitleAndDescription;
-  prismicPayContentsBodyWideBanner: GatsbyTypes.PrismicPayContentsBodyWideBanner;
-  prismicPayContents: GatsbyTypes.PrismicPayContents;
-}>;
+// type IndexPageProps = PageProps<{
+//   prismicPayContentsBodyHero: GatsbyTypes.PrismicPayContentsBodyHero;
+//   prismicPayContentsBodyTitleAndIllustration: GatsbyTypes.PrismicPayContentsBodyTitleAndIllustration;
+//   prismicPayContentsBodyTitleAndDescription: GatsbyTypes.PrismicPayContentsBodyTitleAndDescription;
+//   prismicPayContentsBodyWideBanner: GatsbyTypes.PrismicPayContentsBodyWideBanner;
+//   prismicPayContents: GatsbyTypes.PrismicPayContents;
+// }>;
 
-export const query = graphql`
-  {
-    prismicPayContentsBodyHero {
-      primary {
-        image {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(width: 1920)
-            }
-          }
-        }
-      }
-    }
-    prismicPayContentsBodyTitleAndIllustration {
-      primary {
-        sub_title {
-          text
-        }
-        title {
-          text
-        }
-        illustration {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(width: 700)
-            }
-          }
-        }
-      }
-    }
-    prismicPayContentsBodyTitleAndDescription {
-      primary {
-        title {
-          text
-        }
-        key_text
-      }
-    }
-    prismicPayContentsBodyWideBanner {
-      primary {
-        title {
-          text
-        }
-        thumbnail {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(width: 1920)
-            }
-          }
-        }
-      }
-    }
-    prismicPayContents {
-      data {
-        logo_icon {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(width: 24)
-            }
-          }
-        }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   {
+//     prismicPayContentsBodyHero {
+//       primary {
+//         image {
+//           localFile {
+//             childImageSharp {
+//               gatsbyImageData(width: 1920)
+//             }
+//           }
+//         }
+//       }
+//     }
+//     prismicPayContentsBodyTitleAndIllustration {
+//       primary {
+//         sub_title {
+//           text
+//         }
+//         title {
+//           text
+//         }
+//         illustration {
+//           localFile {
+//             childImageSharp {
+//               gatsbyImageData(width: 700)
+//             }
+//           }
+//         }
+//       }
+//     }
+//     prismicPayContentsBodyTitleAndDescription {
+//       primary {
+//         title {
+//           text
+//         }
+//         key_text
+//       }
+//     }
+//     prismicPayContentsBodyWideBanner {
+//       primary {
+//         title {
+//           text
+//         }
+//         thumbnail {
+//           localFile {
+//             childImageSharp {
+//               gatsbyImageData(width: 1920)
+//             }
+//           }
+//         }
+//       }
+//     }
+//     prismicPayContents {
+//       data {
+//         logo_icon {
+//           localFile {
+//             childImageSharp {
+//               gatsbyImageData(width: 24)
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
 
-const IndexPage: React.FC<IndexPageProps> = ({ data }) => {
-  globalStyles();
+// const IndexPage: React.FC<IndexPageProps> = ({ data }) => {
+//   globalStyles();
 
-  const siteOrigin = useSiteOrigin();
+//   return (
+//     <>
+//       <JsonLd />
+//       <Section>
+//         <Header
+//           image_data={
+//             data.prismicPayContents.data?.logo_icon?.localFile?.childImageSharp
+//               ?.gatsbyImageData
+//           }
+//           alt={data.prismicPayContents.data?.logo_icon?.alt}
+//         />
+//         <HeroSection
+//           image_data={
+//             data.prismicPayContentsBodyHero.primary?.image?.localFile
+//               ?.childImageSharp?.gatsbyImageData
+//           }
+//           alt={data.prismicPayContentsBodyHero.primary?.image?.alt}
+//           title_key_text={
+//             data.prismicPayContentsBodyTitleAndDescription.primary?.key_text
+//           }
+//           title={
+//             data.prismicPayContentsBodyTitleAndDescription.primary?.title?.text
+//           }
+//         />
+//         <TitleAndIllustration
+//           image_data={
+//             data.prismicPayContentsBodyTitleAndIllustration.primary
+//               ?.illustration?.localFile?.childImageSharp?.gatsbyImageData
+//           }
+//           title={
+//             data.prismicPayContentsBodyTitleAndIllustration.primary?.title?.text
+//           }
+//           sub_title={
+//             data.prismicPayContentsBodyTitleAndIllustration.primary?.sub_title
+//               ?.text
+//           }
+//         />
+//         <WideBanner
+//           image_data={
+//             data.prismicPayContentsBodyWideBanner.primary?.thumbnail?.localFile
+//               ?.childImageSharp?.gatsbyImageData
+//           }
+//           title={data.prismicPayContentsBodyWideBanner.primary?.title?.text}
+//         />
+//         <Footer />
+//       </Section>
+//     </>
+//   );
+// };
 
-  return (
-    <>
-      <LogoJsonLd
-        key="metadata"
-        url="https://www.daangnpay.com"
-        logo={siteOrigin + logoUrl}
-      />
-      <Section key="component">
-        <Header
-          image_data={
-            data.prismicPayContents.data?.logo_icon?.localFile?.childImageSharp
-              ?.gatsbyImageData
-          }
-          alt={data.prismicPayContents.data?.logo_icon?.alt}
-        />
-        <HeroSection
-          image_data={
-            data.prismicPayContentsBodyHero.primary?.image?.localFile
-              ?.childImageSharp?.gatsbyImageData
-          }
-          alt={data.prismicPayContentsBodyHero.primary?.image?.alt}
-          title_key_text={
-            data.prismicPayContentsBodyTitleAndDescription.primary?.key_text
-          }
-          title={
-            data.prismicPayContentsBodyTitleAndDescription.primary?.title?.text
-          }
-        />
-        <TitleAndIllustration
-          image_data={
-            data.prismicPayContentsBodyTitleAndIllustration.primary
-              ?.illustration?.localFile?.childImageSharp?.gatsbyImageData
-          }
-          title={
-            data.prismicPayContentsBodyTitleAndIllustration.primary?.title?.text
-          }
-          sub_title={
-            data.prismicPayContentsBodyTitleAndIllustration.primary?.sub_title
-              ?.text
-          }
-        />
-        <WideBanner
-          image_data={
-            data.prismicPayContentsBodyWideBanner.primary?.thumbnail?.localFile
-              ?.childImageSharp?.gatsbyImageData
-          }
-          title={data.prismicPayContentsBodyWideBanner.primary?.title?.text}
-        />
-        <Footer />
-      </Section>
-    </>
-  );
-};
+const IndexPage: React.FC = () => <div>hello world</div>;
+
 
 const Section = styled("section", {
   height: "582px",
