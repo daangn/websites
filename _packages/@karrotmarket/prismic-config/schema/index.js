f10c84d074dabffd8623a6d3fb3e1999275acccd
Hyeseong Kim <tim@daangn.com>
2021-08-03 14:08:33 +0900

    prismic-config: teams_article 컨텐츠 스키마 추가

--- _packages/@karrotmarket/prismic-config/schema/index.js
+++ _packages/@karrotmarket/prismic-config/schema/index.js
@@ -9,5 +9,6 @@ module.exports = {
   pay_contents: require('./pay_contents.json'),
   site_navigation: require('./site_navigation.json'),
   team_contents: require('./team_contents.json'),
+  teams_article: require('./teams_article.json'),
   terms_and_conditions: require('./terms_and_conditions.json'),
 };
