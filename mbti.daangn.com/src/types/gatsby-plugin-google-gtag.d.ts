ba018437b29ed0890f844c8c42419c18f4367e04
Stumpark <woody@daangn.com>
2021-08-12 14:46:41 +0900

    remove mbti directory and private package (#170)
    * remove mbti directory and private package
    
    * restore: prismic-config mbti schema
    
    Resolves #130

--- mbti.daangn.com/src/types/gatsby-plugin-google-gtag.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface Window {
-  // Injected by Google Tag Manager
-  // gtag(action: 'js', at: Date): void
-  // gtag(action: 'config', trackingId: string): void
-
-  gtag(action: 'event', category: string, data?: any): void
-}
