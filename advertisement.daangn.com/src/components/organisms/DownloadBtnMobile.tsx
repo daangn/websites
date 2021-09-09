486913e927387c3798511000544135a877b237a3
Aaron <aaron@daangn.com>
2021-09-09 19:15:32 +0900

    feat(advertisement): add market download link for mobile button
    Target link depends on user agent(OS)

--- advertisement.daangn.com/src/components/organisms/DownloadBtnMobile.tsx
+++ advertisement.daangn.com/src/components/organisms/DownloadBtnMobile.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import { styled } from "~/gatsby-theme-stitches/stitches.config";
 import { rem } from "polished";
+import { isAndroid, isIOS } from "react-device-detect";
 
 export const DownloadBtnMobile: React.FC = () => {
   const [y, setY] = React.useState(0);
@@ -26,12 +27,23 @@ export const DownloadBtnMobile: React.FC = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, [y]);
 
-  // How to detect weather userAgent is android or IOS?
-  const onClickHandler = () => {};
+  const onClickHandler = React.useCallback(() => {
+    if (isIOS)
+      window.open(
+        "https://apps.apple.com/kr/app/%EB%8B%B9%EA%B7%BC%EB%A7%88%EC%BC%93-%EB%8C%80%ED%95%9C%EB%AF%BC%EA%B5%AD-1%EB%93%B1-%EB%8F%99%EB%84%A4-%EC%BB%A4%EB%AE%A4%EB%8B%88%ED%8B%B0/id1018769995",
+        "_blank"
+      );
+    else if (isAndroid)
+      window.open(
+        "https://play.google.com/store/apps/details?id=com.towneers.www",
+        "_blank"
+      );
+    else window.open("https://www.daangn.com/", "_blank"); // userAgent 가 판명되지 않은 기타 경우 당근 홈페이지로 리다이렉트
+  }, []);
 
   return (
     <Wrapper direction={direction}>
-      <Button>당근마켓 앱 다운로드</Button>
+      <Button onClick={() => onClickHandler()}>당근마켓 앱 다운로드</Button>
     </Wrapper>
   );
 };
