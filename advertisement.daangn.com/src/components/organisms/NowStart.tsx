7c6fccd67f56c3ccfa8186319d1b179e350a8c13
Aaron <aaron@daangn.com>
2021-09-13 01:00:01 +0900

    chore(advertisement): apply linting

--- advertisement.daangn.com/src/components/organisms/NowStart.tsx
+++ advertisement.daangn.com/src/components/organisms/NowStart.tsx
@@ -1,9 +1,9 @@
-import React from "react"
-import {styled} from "~/gatsby-theme-stitches/stitches.config"
-import {Section} from "~/components/molecules/Section";
-import {StaticImage as Picture} from "gatsby-plugin-image";
+import React from "react";
+import { styled } from "~/gatsby-theme-stitches/stitches.config";
+import { Section } from "~/components/molecules/Section";
+import { StaticImage as Picture } from "gatsby-plugin-image";
 
-import {rem} from "polished";
+import { rem } from "polished";
 
 export const NowStart = () => {
   return (
@@ -16,41 +16,29 @@ export const NowStart = () => {
           <ImgContainer>
             <Picture src="../../image/img_step_01.png" alt="" width={240} />
             <ImgSubText>
-              <StepNumber>
-                01
-              </StepNumber>
-              <p>
-                나의 당근 &gt; 지역광고 메뉴에서
-              </p>
+              <StepNumber>01</StepNumber>
+              <p>나의 당근 &gt; 지역광고 메뉴에서</p>
             </ImgSubText>
           </ImgContainer>
           <ImgContainer>
             <Picture src="../../image/img_step_02.png" alt="" width={240} />
             <ImgSubText>
-              <StepNumber>
-                02
-              </StepNumber>
-              <p>
-                광고할 프로필을 선택하고
-              </p>
+              <StepNumber>02</StepNumber>
+              <p>광고할 계정을 선택하고</p>
             </ImgSubText>
           </ImgContainer>
           <ImgContainer>
             <Picture src="../../image/img_step_03.png" alt="" width={240} />
             <ImgSubText>
-              <StepNumber>
-                03
-              </StepNumber>
-              <p>
-                광고 만들기를 눌러주세요
-              </p>
+              <StepNumber>03</StepNumber>
+              <p>광고 만들기를 눌러주세요</p>
             </ImgSubText>
           </ImgContainer>
         </ImgRow>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
 const Container = styled("div", {
   display: "grid",
@@ -61,26 +49,26 @@ const Container = styled("div", {
   "@md": {
     paddingX: 0,
     width: "60%",
-    gridTemplateRows: "repeat(2, auto)"
-  }
-})
+    gridTemplateRows: "repeat(2, auto)",
+  },
+});
 
-const GridRow = styled("div",{
-  display: 'grid',
+const GridRow = styled("div", {
+  display: "grid",
   gridTemplateRows: "auto",
-  alignItems: 'center',
-  justifyItems: 'center',
+  alignItems: "center",
+  justifyItems: "center",
   marginBottom: rem(12),
   Subtitle: {
     fontSize: "$body2",
-    fontWeight: 400
+    fontWeight: 400,
   },
 
-  "@md" : {
-    justifyItems: 'start',
+  "@md": {
+    justifyItems: "start",
     marginBottom: rem(20),
-  }
-})
+  },
+});
 
 const TitleBig = styled("p", {
   fontSize: "$subtitle2",
@@ -88,9 +76,9 @@ const TitleBig = styled("p", {
 
   "@md": {
     fontSize: rem(42),
-    lineHeight: rem(52)
-  }
-})
+    lineHeight: rem(52),
+  },
+});
 
 const ImgRow = styled("div", {
   display: "flex",
@@ -99,16 +87,16 @@ const ImgRow = styled("div", {
   alignItems: "center",
   "@md": {
     flexDirection: "row",
-    justifyContent: "space-around"
-  }
-})
+    justifyContent: "space-around",
+  },
+});
 
 const ImgContainer = styled("div", {
   // style={{margin: ` ${rem(43)} 0`}}
-  marginY: rem(43)
-})
+  marginY: rem(43),
+});
 
-const ImgSubText = styled("div",{
+const ImgSubText = styled("div", {
   display: "flex",
   flexDirection: "column",
   justifyContent: "center",
@@ -116,13 +104,13 @@ const ImgSubText = styled("div",{
   marginTop: rem(30),
   p: {
     fontSize: "$subtitle3",
-    fontWeight: 400
-  }
-})
+    fontWeight: 400,
+  },
+});
 
 const StepNumber = styled("p", {
   fontSize: "$subtitle3",
   fontWeight: 700,
   color: "$carrot500",
-  marginY: rem(4)
-})
+  marginY: rem(4),
+});
