b4e5071670af955816b0c549f99de034842dab17
Aaron <aaron@daangn.com>
2021-09-14 10:21:30 +0900

    chore(advertisement): remove unused files

--- advertisement.daangn.com/src/components/molecules/fbTrack.tsx
+++
@@ -1,7 +0,0 @@
-export default function fbTrack (a: string, b: string) {
-  // @ts-ignore
-  if(typeof fbq === `function`){
-    // @ts-ignore
-    fbq(a, b)
-  }
-}
--- advertisement.daangn.com/src/components/molecules/gtag.ts
+++
@@ -1,8 +0,0 @@
-export const gtag = (event: string, type: string, data?: any) => {
-  try {
-    // @ts-ignore
-    typeof window !== "undefined" && window.gtag(event, type, data);
-  } catch (e) {
-    console.warn("GA script not found");
-  }
-};
