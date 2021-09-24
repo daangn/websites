0a8fa2a0b8212d61626d177f1b401400c704f2b7
Aaron <aaron@daangn.com>
2021-09-24 17:40:47 +0900

    chore(advertisement): change naver site verification token

--- ads-local.daangn.com/src/html.js
+++ ads-local.daangn.com/src/html.js
@@ -11,7 +11,7 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <meta name="naver-site-verification" content="da2894a9f1a3b009d2de6a6a5c8fd467a40687ac" />
+        <meta name="naver-site-verification" content="270bbdd42dd2751190d4755c4a1ca68b41538a62" />
         <meta name="description" content="동네 이웃들이 모이는 당근마켓에 광고해 보세요"/>
         <script
           dangerouslySetInnerHTML={{
