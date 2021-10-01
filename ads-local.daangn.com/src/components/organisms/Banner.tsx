272ad0f8ced28f879c5c479a8c1b70c70e9d8c74
Yeonggyun Kim <aaron@daangn.com>
2021-10-01 17:37:07 +0900

    fix(ads-local): support webp file for banner image (#289)
    * chore(advertisement): change naver site verification token
    
    * fix(advertisement): fix meta tags not working
    
    * fix(advertisement): change onClick handler trigger position
    
    * fix(advertisement): reduce image size
    
    * fix(ads-local): change pc interview image
    
    * fix(ads-local): Reduce banner image size
    
    * fix(ads-local): remove duplicated import

--- ads-local.daangn.com/src/components/organisms/Banner.tsx
+++ ads-local.daangn.com/src/components/organisms/Banner.tsx
@@ -1,14 +1,27 @@
-import * as React from "react";
+import banner_avif from '~/image/img_cover_mobile.avif'
+import banner_webp from '~/image/img_cover_mobile.webp'
+
+import banner_pc_avif from '~/image/img_cover_pc.avif'
+import banner_pc_webp from '~/image/img_cover_pc.webp'
 
 import advertisementMobileBanner from "~/image/img_cover_mobile.png";
 import advertisementPcBanner from "~/image/img_cover_pc.png";
+
 import { styled } from "gatsby-theme-stitches/src/stitches.config";
 import { DownloadButton } from "~/components/molecules/DownloadButton";
-import { rem } from "polished";
+import { rem, em } from "polished";
 
 export function Banner() {
   return (
     <Background>
+      <picture>
+        <source media={`(max-width: ${em(768)})`} srcSet={banner_avif} type="image/avif"/>
+        <source media={`(max-width: ${em(768)})`} srcSet={banner_webp} type="image/webp"/>
+        <source media={`(min-width: ${em(768)})`} srcSet={banner_pc_avif} type="image/avif"/>
+        <source media={`(min-width: ${em(768)})`} srcSet={banner_pc_webp} type="image/webp"/>
+        <ImgPc src={advertisementPcBanner} srcSet={`${advertisementMobileBanner} 720w,
+          ${advertisementPcBanner} 3761w`} />
+      </picture>
       <Wrapper>
         <Description>
           동네 이웃들이 모이는
@@ -28,19 +41,36 @@ export function Banner() {
   );
 }
 
+const ImgPc = styled('img', {
+  position: 'absolute',
+  minHeight: rem(600),
+  width: '100%',
+  height: "100%",
+  top: 0,
+  right: 0,
+  zIndex: -1,
+  objectFit: 'cover',
+  justifyContent: "center",
+  transition: "0.2s ease-in-out",
+  "@md" : {
+    height: rem(780),
+  },
+})
+
+
 const Background = styled("div", {
   display: "flex",
   justifyContent: "center",
   position: "relative",
   height: rem(600),
-  backgroundImage: `url(${advertisementMobileBanner})`,
-  backgroundSize: "cover",
-  backgroundRepeat: "no-repeat",
-  transition: "0.2s ease-in-out",
+  // backgroundImage: `url(${advertisementMobileBanner})`,
+  // backgroundSize: "cover",
+  // backgroundRepeat: "no-repeat",
+  // transition: "0.2s ease-in-out",
   "@md": {
     height: 780,
-    backgroundImage: `url(${advertisementPcBanner})`,
-    backgroundPosition: "center center",
+    // backgroundImage: `url(${advertisementPcBanner})`,
+    // backgroundPosition: "center center",
   },
 });
 
--- ads-local.daangn.com/src/types/file-loader.d.ts
+++ ads-local.daangn.com/src/types/file-loader.d.ts
@@ -7,3 +7,14 @@ declare module '*.jpg' {
   const url: string;
   export default url;
 }
+
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.avif' {
+  const value: string;
+  export default value;
+}
