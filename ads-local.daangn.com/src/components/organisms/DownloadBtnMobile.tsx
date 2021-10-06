6656de4057ddde8b37268bc5d51248c9ccc83cb8
Aaron <aaron@daangn.com>
2021-10-06 17:04:34 +0900

    Update onelink url

--- ads-local.daangn.com/src/components/organisms/DownloadBtnMobile.tsx
+++ ads-local.daangn.com/src/components/organisms/DownloadBtnMobile.tsx
@@ -4,10 +4,16 @@ import { styled } from "~/gatsby-theme-stitches/stitches.config";
 import { rem } from "polished";
 
 export const DownloadBtnMobile: React.FC = () => {
-
   return (
     <Wrapper>
-      <Button onClick={()=>window.open("https://daangn.onelink.me/oWdR/4e25da2f", "_blank")}>당근마켓 앱 다운로드</Button>
+      <a
+        href="https://daangn.onelink.me/oWdR/4e25da2f?af_dp=karrot%3A%2F%2Fminikarrot%2Frouter%3Fapp%3Dhttps%3A%2F%2Fwebview.prod.kr.karrotmarket.com%2Fkotisaari%26path%3D%2Fadvertisements%3Freferral%3Dshare.product.detail%26navbar%3Dfalse%26scrollable%3Dfalse&af_ios_url=karrot%3A%2F%2Fminikarrot%2Frouter%3Fapp%3Dhttps%3A%2F%2Fwebview.prod.kr.karrotmarket.com%2Fkotisaari%26path%3D%2Fadvertisements%3Freferral%3Dshare.product.detail%26navbar%3Dfalse%26scrollable%3Dfalse&af_android_url=karrot://minikarrot/router?app=https://webview.prod.kr.karrotmarket.com/kotisaari&path=/advertisements?referral=share.product.detail&navbar=false&scrollable=false"
+        style={{textDecoration: 'none', color: 'inherit'}}
+      >
+        <Button>
+          당근마켓 앱 다운로드
+        </Button>
+      </a>
     </Wrapper>
   );
 };
