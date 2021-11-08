ae896db8b5a2b89d00eafacd67f6815a5dcb2428
Hyeseong Kim <tim@daangn.com>
2021-11-09 01:16:28 +0900

    안쓰는 코드 제거

--- _packages/@karrotmarket/gatsby-source-greenhouse-job-board/gatsby-node.d.ts
+++
@@ -1,4 +0,0 @@
-import type { GatsbyNode } from 'gatsby';
-export declare const pluginOptionsSchema: GatsbyNode['pluginOptionsSchema'];
-export declare const createSchemaCustomization: GatsbyNode['createSchemaCustomization'];
-export declare const sourceNodes: GatsbyNode['sourceNodes'];
