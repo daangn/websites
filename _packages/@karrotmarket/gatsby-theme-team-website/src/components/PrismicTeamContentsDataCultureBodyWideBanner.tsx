3913c39c6fea2d645c74acfba6edc84b00b0fff1
Hyeseong Kim <tim@daangn.com>
2021-11-09 07:26:20 +0900

    gatsby-theme-team-website: Main <-> Culture 페이지 컴포넌트 교환

---
+++ _packages/@karrotmarket/gatsby-theme-team-website/src/components/PrismicTeamContentsDataCultureBodyWideBanner.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import { rem } from 'polished';
+import { graphql } from 'gatsby';
+import { getImage, GatsbyImage } from 'gatsby-plugin-image';
+import { styled } from 'gatsby-theme-stitches/src/config';
+import { useLinkParser } from '@karrotmarket/gatsby-theme-website/src/link';
+
+import _DetailLink from './DetailLink';
+
+type PrismicTeamContentsDataCultureBodyWideBannerProps = {
+  data: GatsbyTypes.PrismicTeamContentsDataCultureBodyWideBanner_dataFragment,
+  className?: string,
+};
+
+export const query = graphql`
+  fragment PrismicTeamContentsDataCultureBodyWideBanner_data on PrismicTeamContentsDataCultureBodyWideBanner {
+    primary {
+      title {
+        text
+      }
+      thumbnail {
+        alt
+        localFile {
+          childImageSharp {
+            gatsbyImageData(
+              height: 540
+              layout: CONSTRAINED
+              quality: 100
+            )
+          }
+        }
+      }
+      background_color
+      link_text
+      link {
+        url
+      }
+    }
+  }
+`;
+
+const Container = styled('section', {
+});
+
+const BannerImage = styled(GatsbyImage, {
+  width: '100%',
+});
+
+const BannerContainer = styled('section', {
+  display: 'grid',
+  justifyContent: 'center',
+  gap: rem(32),
+  paddingX: rem(64),
+  paddingY: rem(72),
+
+  '@md': {
+    paddingY: rem(100),
+  },
+});
+
+const Title = styled('h1', {
+  textAlign: 'center',
+  typography: '$subtitle2',
+
+  '@md': {
+    typography: '$heading4',
+  },
+});
+
+const DetailLink = styled(_DetailLink, {
+  justifyContent: 'center',
+});
+
+const PrismicTeamContentsDataCultureBodyWideBanner: React.FC<PrismicTeamContentsDataCultureBodyWideBannerProps> = ({
+  data,
+  className,
+}) => {
+  const parseLink = useLinkParser();
+
+  if (data.primary == null) {
+    return null;
+  }
+
+  const thumbnailImage = data.primary.thumbnail?.localFile?.childImageSharp?.gatsbyImageData && getImage(
+    data.primary.thumbnail.localFile.childImageSharp.gatsbyImageData
+  );
+
+  return (
+    <Container className={className}>
+      {thumbnailImage && (
+        <BannerImage
+          image={thumbnailImage}
+          objectFit="cover"
+          objectPosition="center"
+          alt={data.primary.thumbnail?.alt || ''}
+        />
+      )}
+      <BannerContainer
+        css={{
+          backgroundColor: data.primary.background_color,
+        }}
+      >
+        <Title>{data.primary.title?.text}</Title>
+        {data.primary.link?.url && (
+          <DetailLink
+            link={parseLink(data.primary.link.url)}
+            message={data.primary.link_text}
+          />
+        )}
+      </BannerContainer>
+    </Container>
+  );
+};
+
+export default PrismicTeamContentsDataCultureBodyWideBanner;
---
+++ _packages/@karrotmarket/gatsby-theme-team-website/src/components/PrismicTeamContentsDataMainBodyBenefit.tsx
@@ -0,0 +1,150 @@
+import * as React from 'react';
+import { rem } from 'polished';
+import { graphql } from 'gatsby';
+import { styled } from 'gatsby-theme-stitches/src/config';
+
+type PrismicTeamContentsDataMainBodyBenefitProps = {
+  data: GatsbyTypes.PrismicTeamContentsDataMainBodyBenefit_dataFragment,
+  className?: string,
+};
+
+export const query = graphql`
+  fragment PrismicTeamContentsDataMainBodyBenefit_data on PrismicTeamContentsDataMainBodyBenefit {
+    primary {
+      title {
+        text
+      }
+    }
+    items {
+      subtitle
+      subtext
+      description {
+        html
+      }
+    }
+  }
+`;
+
+const Container = styled('article', {
+  contentArea: true,
+  width: '100%',
+  display: 'grid',
+  gap: rem(64),
+
+  '@md': {
+    gap: rem(100),
+  },
+});
+
+const Header = styled('header', {
+  display: 'grid',
+  gap: rem(32),
+});
+
+const Title = styled('h2', {
+  whiteSpace: 'pre-line',
+  typography: '$subtitle2',
+
+  '@md': {
+    typography: '$heading4',
+  },
+});
+
+const BenefitGroupList = styled('ol', {
+  listStyle: 'none',
+  paddingLeft: 0,
+  display: 'grid',
+  gap: rem(48),
+
+  '@lg': {
+    gridTemplateColumns: '1fr 1fr',
+    gap: `${rem(80)} ${rem(144)}`,
+  },
+});
+
+const BenefitGroup = styled('li', {
+  display: 'grid',
+  gridTemplateRows: 'min-content',
+  gap: rem(24),
+});
+
+const BenefitGroupTitle = styled('h3', {
+  display: 'inline-flex',
+  alignItems: 'flex-start',
+  typography: '$subtitle3',
+
+  '@md': {
+    typography: '$subtitle1',
+  },
+
+  '&::before': {
+    content: 'attr(data-num)',
+    color: '$carrot500',
+    fontSize: '0.8em',
+    lineHeight: 1,
+    marginRight: rem(16),
+    transform: 'translateY(50%)',
+
+    '@md': {
+      fontSize: '0.6em',
+    },
+  },
+});
+
+const Benefit = styled('div', {
+  display: 'grid',
+  gap: rem(8),
+
+  '@md': {
+    gap: rem(24),
+  },
+});
+
+const BenefitSummary = styled('p', {
+  typography: '$body2',
+  color: '$gray700',
+});
+
+const BenefitDetail = styled('div', {
+  typography: '$body2',
+  color: '$gray700',
+  '& > ul': {
+    paddingLeft: rem(24),
+    display: 'grid',
+    gap: rem(8),
+  },
+});
+
+const PrismicTeamContentsDataMainBodyBenefit: React.FC<PrismicTeamContentsDataMainBodyBenefitProps> = ({
+  data,
+  className,
+}) => {
+  return (
+    <Container className={className}>
+      <Header>
+        <Title>
+          {data.primary?.title?.text}
+        </Title>
+      </Header>
+      <BenefitGroupList>
+        {(data.items || []).map((item, i) => (
+          <BenefitGroup key={i}>
+            <BenefitGroupTitle data-num={(i + 1).toString().padStart(2, '0')}>
+              {item.subtitle}
+            </BenefitGroupTitle>
+            <Benefit>
+              <BenefitSummary>
+                {item.subtext}
+              </BenefitSummary>
+              <BenefitDetail
+                dangerouslySetInnerHTML={{ __html: item.description?.html || '' }}
+              />
+            </Benefit>
+          </BenefitGroup>
+        ))}
+      </BenefitGroupList>
+    </Container>
+  );
+};
+
+export default PrismicTeamContentsDataMainBodyBenefit;
