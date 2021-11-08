b18e292715bfa52ffa0ae0216db08a942b6a1891
Hyeseong Kim <tim@daangn.com>
2021-11-09 08:09:40 +0900

    글로벌 채용사이트 추가 (#344)

---
+++ careers.en.karrotmarket.com/gatsby/gatsby-config.ts
@@ -0,0 +1,128 @@
+import type { GatsbyConfig } from 'gatsby';
+
+import dotenv from 'dotenv-safe';
+dotenv.config();
+
+const gql = String.raw;
+
+export const siteMetadata = {
+  siteUrl: 'https://careers.en.karrotmarket.com',
+  siteName: 'Karrot Careers',
+};
+
+const config: GatsbyConfig = {
+  // See https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby/src/utils/flags.ts
+  flags: {
+    FAST_DEV: true,
+    QUERY_ON_DEMAND: true,
+    LAZY_IMAGES: true,
+    PARALLEL_SOURCING: true,
+  },
+  siteMetadata,
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: siteMetadata.siteName,
+        short_name: siteMetadata.siteName,
+        start_url: '/',
+        theme_color: '#ff7e36',
+        background_color: '#ffffff',
+        display: 'minimal-ui',
+        icon: 'src/assets/favicon.svg',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-advanced-sitemap',
+      options: {
+        exclude: [
+          '/dev-404-page/',
+          '/404/',
+          '/404.html',
+          '/offline-plugin-app-shell-fallback/',
+          '/preview/',
+          '/completed/',
+          /\/apply\/?$/,
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/plain-sitemap',
+        excludes: [
+          '/dev-404-page/',
+          '/404/',
+          '/404.html',
+          '/offline-plugin-app-shell-fallback/',
+          '/preview/',
+          '/completed/',
+          '/jobs/*/apply/',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: null,
+        sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+        policy: [
+          { userAgent: '*', allow: '/' },
+          { userAgent: '*', disallow: '/dev-404-page/' },
+          { userAgent: '*', disallow: '/404/' },
+          { userAgent: '*', disallow: '/404.html' },
+          { userAgent: '*', disallow: '/offline-plugin-app-shell-fallback/' },
+          { userAgent: '*', disallow: '/preview/' },
+          { userAgent: '*', disallow: '/completed/' },
+          { userAgent: '*', disallow: '/*/apply/$' },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-WVSZ75S',
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: 'gatsby' },
+        routeChangeEventName: 'gatsby-route-change',
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-typegen',
+      options: {
+        outputPath: 'src/__generated__/gatsby-types.d.ts',
+        emitSchema: {
+          'src/__generated__/gatsby-schema.graphql': true,
+          'src/__generated__/gatsby-introspection.json': true,
+        },
+        emitPluginDocuments: {
+          'src/__generated__/gatsby-plugin-documents.graphql': true,
+        },
+      },
+    },
+
+    // 커스텀 플러그인
+    {
+      resolve: '@karrotmarket/gatsby-theme-team-website',
+      options: {
+        locale: 'en',
+        navigationId: 'careers.en.karrotmarket.com',
+      },
+    },
+    {
+      resolve: '@karrotmarket/gatsby-source-greenhouse-jobboard',
+      options: {
+        boardToken: 'karrot',
+        forceGC: true,
+      },
+    },
+    {
+      resolve: '@karrotmarket/gatsby-transformer-job-post',
+      options: {},
+    },
+  ],
+};
+
+export default config;
---
+++ careers.en.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/templates/jobApplicationPage/messages.ts
@@ -0,0 +1,26 @@
+export default {
+  'alert_completed': '지원서가 제출되었습니다. 빠른 시일 내에 검토 후 연락드리겠습니다.',
+  'alert_failed': '지원서를 제출하지 못했습니다. 문제가 지속되는 경우 recruit@daangn.com 으로 문의주시면 도움드리겠습니다',
+
+  'field_name_label': 'Full name',
+  'field_name_placeholder': 'Enter full name',
+
+  'field_phone_label': 'Phone number',
+  'field_phone_placeholder': 'Enter actual phone number',
+
+  'field_email_label': 'Email address',
+  'field_email_placeholder': 'Enter actual email address',
+
+  'field_resume_label': 'Resume',
+  'field_resume_placeholder': 'Attach file',
+  'field_resume_description': 'Only pdf, doc, docx, txt, rtf formats are allowed',
+
+  'field_portfolio_label': 'Cover letter',
+  'field_portfolio_placeholder': 'Attach file',
+  'field_portfolio_description': '* Up to 50MiB',
+
+  'terms_privacy_info': 'Privacy policy',
+  'terms_sensitive_info': 'Sensitive information policy',
+
+  'button_submit': 'Agree and apply',
+};
