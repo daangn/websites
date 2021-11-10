603550b1d25c68335033f331cea6d465bcfb363c
Hyeseong Kim <tim@daangn.com>
2021-11-10 15:30:07 +0900

    careers.en.karrotkarket.com: 채용 공고 목록 번역 메시지 변경

--- careers.en.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/templates/jobsPage/messages.ts
+++ careers.en.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/templates/jobsPage/messages.ts
@@ -1,13 +1,13 @@
 export default {
   'meta_title_default': 'Karrot Careers',
 
-  'chapter_all': 'All Positions',
+  'chapter_all': 'Job Fields',
 
-  'employment_type_all': 'All Employments',
-  'employment_type_fulltime': 'Full time',
-  'employment_type_contractor': 'Contractor',
+  'employment_type_all': 'Employment Type',
+  'employment_type_fulltime': 'Full-time',
+  'employment_type_contractor': 'Contract',
   'employment_type_intern': 'Intern',
-  'employment_type_assistant': 'Assistant',
+  'employment_type_assistant': 'Temporary',
 
   'search_placeholder': 'Search',
 };
