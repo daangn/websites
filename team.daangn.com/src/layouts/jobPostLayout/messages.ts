752affa45958d63bbae7dacf1f14d8ecdfe8ccc7
Hyeseong Kim <tim@daangn.com>
2021-11-12 01:32:38 +0900

    메시지 경로 수정

---
+++ team.daangn.com/src/layouts/jobPostLayout/messages.ts
@@ -0,0 +1,20 @@
+export default {
+  'corporate_karrot_market': '당근마켓',
+  'corporate_karrot_market_url': 'https://www.daangn.com',
+  'corporate_karrot_pay': '당근페이',
+  'corporate_karrot_pay_url': 'https://www.daangnpay.com',
+
+  'employment_type_full_time': '정규직',
+  'employment_type_contractor': '계약직',
+  'employment_type_intern': '인턴',
+  'employment_type_assistant': '어시스턴트',
+
+  'experience_yes': '경력',
+  'experience_no': '신입',
+  'experience_whatever': '경력/신입',
+
+  'back_to_list': '목록으로 돌아가기',
+
+  'tab_view': '영입정보',
+  'tab_apply': '지원하기',
+};
