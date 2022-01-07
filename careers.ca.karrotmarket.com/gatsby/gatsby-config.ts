007d9e0bff914cebe8394ecfd34d1b790bf74508
Hyeseong Kim <tim@daangn.com>
2022-01-07 20:05:47 +0900

    careers.en.karrotmarket.com -> careers.ca.karrotmarket.com 도메인 이전을 위한 복제

---
+++ careers.ca.karrotmarket.com/gatsby/gatsby-config.ts
@@ -0,0 +1,131 @@
+import type { GatsbyConfig } from 'gatsby';
+
+import dotenv from 'dotenv-safe';
+dotenv.config();
+
+const gql = String.raw;
+
+export const siteMetadata = {
+  siteUrl: 'https://careers.ca.karrotmarket.com',
+  siteName: 'Karrot Careers',
+};
+
+const config: GatsbyConfig = {
+  siteMetadata,
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: siteMetadata.siteName,
+        short_name: siteMetadata.siteName,
+        start_url: '/',
+        theme_color: '#ff7e36',
+        background_color: '#ffffff',
+        display: 'minimal-ui',
+        icon: 'src/assets/favicon.svg',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-advanced-sitemap',
+      options: {
+        exclude: [
+          '/dev-404-page/',
+          '/404/',
+          '/404.html',
+          '/offline-plugin-app-shell-fallback/',
+          '/preview/',
+          '/completed/',
+          /\/apply\/?$/,
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/plain-sitemap',
+        excludes: [
+          '/dev-404-page/',
+          '/404/',
+          '/404.html',
+          '/offline-plugin-app-shell-fallback/',
+          '/preview/',
+          '/completed/',
+          '/jobs/*/apply/',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: null,
+        sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+        policy: [
+          { userAgent: '*', allow: '/' },
+          { userAgent: '*', disallow: '/dev-404-page/' },
+          { userAgent: '*', disallow: '/404/' },
+          { userAgent: '*', disallow: '/404.html' },
+          { userAgent: '*', disallow: '/offline-plugin-app-shell-fallback/' },
+          { userAgent: '*', disallow: '/preview/' },
+          { userAgent: '*', disallow: '/completed/' },
+          { userAgent: '*', disallow: '/*/apply/$' },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-WVSZ75S',
+        includeInDevelopment: false,
+        defaultDataLayer: { platform: 'gatsby' },
+        routeChangeEventName: 'gatsby-route-change',
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-typegen',
+      options: {
+        outputPath: 'src/__generated__/gatsby-types.d.ts',
+        emitSchema: {
+          'src/__generated__/gatsby-schema.graphql': true,
+          'src/__generated__/gatsby-introspection.json': true,
+        },
+        emitPluginDocuments: {
+          'src/__generated__/gatsby-plugin-documents.graphql': true,
+        },
+      },
+    },
+
+    // 커스텀 플러그인
+    {
+      resolve: '@karrotmarket/gatsby-theme-team-website',
+      options: {
+        locale: 'en',
+        navigationId: 'careers.ca.karrotmarket.com',
+      },
+    },
+    {
+      resolve: '@karrotmarket/gatsby-source-greenhouse-jobboard',
+      options: {
+        boardToken: 'karrot',
+        forceGC: true,
+      },
+    },
+    process.env.NODE_ENV === 'development' && {
+      resolve: '@karrotmarket/gatsby-source-greenhouse-jobboard',
+      options: {
+        boardToken: 'daangntest1',
+      },
+    },
+    {
+      resolve: '@karrotmarket/gatsby-transformer-job-post',
+      options: {
+        defaultTags: {
+          'daangntest1': ['개발 테스트'],
+        },
+      },
+    },
+  ].filter(Boolean),
+};
+
+export default config;
---
+++ careers.ca.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/layouts/jobPostLayout/messages.ts
@@ -0,0 +1,20 @@
+export default {
+  corporate_karrot_market: 'Karrot',
+  corporate_karrot_market_url: '',
+  corporate_karrot_pay: 'Karrot Payment',
+  corporate_karrot_pay_url: '',
+
+  employment_type_full_time: 'Full-time',
+  employment_type_contractor: 'Contract',
+  employment_type_intern: 'Intern',
+  employment_type_assistant: 'Temporary',
+
+  experience_yes: '',
+  experience_no: '',
+  experience_whatever: '',
+
+  back_to_list: 'Back to list',
+
+  tab_view: 'Job Description',
+  tab_apply: 'Apply',
+};
