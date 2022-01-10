7957142c40c760ed3da5c94594731837e15e8f8d
Hyeseong Kim <tim@daangn.com>
2022-01-10 21:35:22 +0900

    init careers.jp.karrotmarket.com

---
+++ careers.jp.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/components/jobPostList/emptyPlaceholder/messages.ts
@@ -0,0 +1,4 @@
+export default {
+  'no_results': 'No search results.',
+  'back_to_list': 'Clear filters',
+};
---
+++ careers.jp.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/layouts/jobPostLayout/JobPostingJsonLd.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const JobPostingJsonLd: React.FC = () => {
+  // ~~아직 지원 안함. Greenhouse 에서 Location 이던 Department 던 공고에 메타데이터 다 넣어야 될듯~~
+  // TODO: ca 도메인으로 이전 후에는 하드코딩으로라도 대응 가능함
+  return null;
+};
+
+export default JobPostingJsonLd;
