405cbee5a18aaf39ccc483f961ec825ac3ab3a12
Hyeseong Kim <tim@daangn.com>
2022-01-10 21:05:50 +0900

    안쓰는거 다 지워

--- careers.en.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/components/formField/messages.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  attach_file: 'Attach a file',
-
-  select_empty: '(Click to select)',
-  select_yes: 'Yes',
-  select_no: 'No',
-};
--- careers.en.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/layouts/jobPostLayout/JobPostingJsonLd.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-
-const JobPostingJsonLd: React.FC = () => {
-  // 아직 지원 안함. Greenhouse 에서 Location 이던 Department 던 공고에 메타데이터 다 넣어야 될듯
-  return null;
-};
-
-export default JobPostingJsonLd;
