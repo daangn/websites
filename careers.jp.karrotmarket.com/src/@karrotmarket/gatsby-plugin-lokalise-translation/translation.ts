20c91dff51a9d99679bfa87c57d4c4844c8a4250
Hyeseong Kim <tim@daangn.com>
2022-02-09 15:39:56 +0900

    chore: typegen

--- careers.jp.karrotmarket.com/src/@karrotmarket/gatsby-plugin-lokalise-translation/translation.ts
+++ careers.jp.karrotmarket.com/src/@karrotmarket/gatsby-plugin-lokalise-translation/translation.ts
@@ -1,7 +1,7 @@
 import { graphql, useStaticQuery } from 'gatsby';
 
 export function useTranslation() {
-  const data = useStaticQuery(graphql`
+  const data = useStaticQuery<GatsbyTypes.LokaliseTranslationQuery>(graphql`
     query LokaliseTranslation {
       lokaliseTranslation(locale: {regex: "/jp/i"}) {
         messages {
