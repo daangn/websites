c3ddcdfc55bd2327a4492387f55f9dca07cd0fcb
ginger-kang <kdhoon07@gmail.com>
2022-03-28 11:43:46 +0900

    feat: Search 공용 컴포넌트로 분리

--- _packages/@karrotmarket/gatsby-theme-team-website/src/templates/JobsPage.tsx
+++ _packages/@karrotmarket/gatsby-theme-team-website/src/templates/JobsPage.tsx
@@ -11,6 +11,7 @@ import { ReactComponent as SearchdSvg } from '../assets/searchOutlineM.svg';
 
 import PageTitle from '../components/PageTitle';
 import _JobPostList from '../components/JobPostList';
+import Search from '../components/Search'
 import expandMoreOutlineUrl from '!!file-loader!../assets/expand_more_outline_m.svg';
 import { useFlexSearch } from '../utils/useFlexSearch';
 
@@ -199,45 +200,6 @@ const Select = styled('select', {
     height: '0.5em',
     clipPath: 'polygon(100% 0%, 0 0%, 50% 100%)',
     background: '$gray500',
-  },
-});
-
-const Search = styled('div', {
-  gridArea: 'search',
-  display: 'inline-flex',
-  position: 'relative',
-  alignItems: 'center',
-  width: '100%',
-
-  '@lg': {
-    width: 'auto',
-    marginLeft: 'auto',
-  },
-
-  '& > input':{
-    flex:1,
-    alignItems: 'center',
-    height: rem(52),
-    border: '1px solid $gray400',
-    boxSizing: 'border-box',
-    borderRadius: rem(8),
-    typography: '$body2',
-    paddingLeft: rem(52),
-    paddingRight: rem(14),
-    '::placeholder': {
-      color: '$gray500',
-    },
-    '&:focus': {
-      border: '1px solid $carrot500',
-    },
-    '&:focus~svg': {
-      color: '$carrot500',
-    },
-  },
-  '& > svg':{
-    color:'$gray400',
-    position:'absolute',
-    left:rem(20),
   },
 });
 
