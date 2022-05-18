f41f787e773d97da861dd53953b297ab83fae0d2
ginger-kang <kdhoon07@gmail.com>
2022-05-18 22:20:22 +0900

    global corporation transition

--- careers.jp.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/components/jobPostSummary/generateProperties.ts
+++ careers.jp.karrotmarket.com/src/@karrotmarket/gatsby-theme-team-website/components/jobPostSummary/generateProperties.ts
@@ -1,13 +1,15 @@
 type JobPostLike = {
-  corporate?: string,
+  corporate?: {
+    type: string
+  },
 }
 
 export default function *generateProperties(jobPost: JobPostLike): Generator<string, void, unknown> {
   console.log(jobPost);
-  if (jobPost.corporate) {
+  if (jobPost.corporate?.type) {
     yield {
       'KARROT_MARKET': 'Karrot',
       'KARROT_PAY': 'Karrot Pay',
-    }[jobPost.corporate] || 'Karrot';
+    }[jobPost.corporate?.type] || 'Karrot';
   }
 }
