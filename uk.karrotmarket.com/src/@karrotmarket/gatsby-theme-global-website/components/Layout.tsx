3f2bae0bbbac99297cc9e9700f25dfca979d8d56
Hyeseong Kim <tim@daangn.com>
2022-10-12 15:16:54 +0900

    fix uk build

--- uk.karrotmarket.com/src/@karrotmarket/gatsby-theme-global-website/components/Layout.tsx
+++ uk.karrotmarket.com/src/@karrotmarket/gatsby-theme-global-website/components/Layout.tsx
@@ -24,28 +24,26 @@ const Layout: React.FC<LayoutProps> = ({
   data,
   id,
 }) => {
-  if (!data) return <></>;
-
   globalStyles();
+
+  if (!data) return <>{children}</>;
 
   return (
     <>
-      <Helmet key="helmet">
-        <html lang="en" data-seed="light-only" data-seed-scale-color="light" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1"
-        />
-        <meta name="color-scheme" content="light dark" />
-      </Helmet>
       <Header
         key="header"
         navigationData={data}
         sns
       />
-      <main {...(id ? { id } : {})}>{children}</main>
 
-      <Footer key="footer" navigationData={data} />
+      <main {...(id ? { id } : {})}>
+        {children}
+      </main>
+
+      <Footer
+        key="footer"
+        navigationData={data}
+      />
     </>
   );
 };
