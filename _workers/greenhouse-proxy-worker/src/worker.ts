33362d794ca351d93c173f9000f014ec3aaf99b4
Hyeseong Kim <tim@daangn.com>
2022-11-19 17:57:30 +0900

    add turnstile feature flag, and disable temporarly

--- _workers/greenhouse-proxy-worker/src/worker.ts
+++ _workers/greenhouse-proxy-worker/src/worker.ts
@@ -12,22 +12,27 @@ import * as Cache from 'worktop/cfw.cache';
 interface Context extends WorktopContext {
   bindings: {
     /**
-    * Greenhouse Jobboard API Key
-    *
-    * wrangler secret 커맨드로 관리합니다.
-    *
-    * @See https://app3.greenhouse.io/configure/dev_center/credentials
-    * @See https://developers.cloudflare.com/workers/cli-wrangler/commands#secret
-    */
+     * Greenhouse Jobboard API Key
+     *
+     * wrangler secret 커맨드로 관리합니다.
+     *
+     * @See https://app3.greenhouse.io/configure/dev_center/credentials
+     * @See https://developers.cloudflare.com/workers/cli-wrangler/commands#secret
+     */
     GH_JOBBOARD_API_KEY: string;
 
     /**
-    * Cloudflare Turnstile secret key
-    *
-    * wrangler secret 커맨드로 관리합니다.
-    *
-    * @See https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
-    */
+     * Cloudflare Turnstile flag
+     */
+    TURNSTILE_ENABLED: boolean;
+
+    /**
+     * Cloudflare Turnstile secret key
+     *
+     * wrangler secret 커맨드로 관리합니다.
+     *
+     * @See https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
+     */
     TURNSTILE_SECRET_KEY: string;
   },
 }
@@ -48,34 +53,36 @@ API.add('POST', '/boards/:boardToken/jobs/:jobId/application/proxy', async (req,
   const greenhouseEndpoint = `https://boards-api.greenhouse.io/v1/boards/${boardToken}/jobs/${jobId}`;
   const requestBody = await req.formData();
 
-  const token = requestBody.get('cf-turnstile-response');
-  const ip = req.headers.get('CF-Connecting-IP');
-  if (token) {
-    try {
-      const formData = new FormData();
-      formData.append('secret', ctx.bindings.TURNSTILE_SECRET_KEY);
-      formData.append('response', token);
-      if (ip) {
-        formData.append('remoteip', ip);
-      }
+  if (ctx.bindings.TURNSTILE_ENABLED) {
+    const token = requestBody.get('cf-turnstile-response');
+    const ip = req.headers.get('CF-Connecting-IP');
+    if (token) {
+      try {
+        const formData = new FormData();
+        formData.append('secret', ctx.bindings.TURNSTILE_SECRET_KEY);
+        formData.append('response', token);
+        if (ip) {
+          formData.append('remoteip', ip);
+        }
 
-      const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
-      const response = await fetch(url, {
-        body: formData,
-        method: 'POST',
-      });
-      const data = await response.json();
-      if (data.success !== true) {
-        return reply(403, 'Are you human? Otherwise automated request does not allowed :(');
+        const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
+        const response = await fetch(url, {
+          body: formData,
+          method: 'POST',
+        });
+        const data = await response.json();
+        if (data.success !== true) {
+          return reply(403, 'Are you human? Otherwise automated request does not allowed :(');
+        }
+      }  catch (error) {
+        if (error instanceof Error) {
+          return reply(500, error.message);
+        }
+        return reply(500, 'Internal Server Error');
       }
-    }  catch (error) {
-      if (error instanceof Error) {
-        return reply(500, error.message);
-      }
-      return reply(500, 'Internal Server Error');
+    } else {
+      return reply(400, 'CAPTCHA (Turnstile) token does not set');
     }
-  } else {
-    return reply(400, 'CAPTCHA (Turnstile) token does not set');
   }
 
   try {
