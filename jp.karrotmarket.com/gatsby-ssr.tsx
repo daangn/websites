16a164d61076c575196777b26998be9e9bbdfd54
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2022-12-13 17:17:53 +0900

    chore: remove unused ssr logic (#772)

--- jp.karrotmarket.com/gatsby-ssr.tsx
+++ jp.karrotmarket.com/gatsby-ssr.tsx
@@ -1,19 +1,9 @@
 import { type GatsbySSR } from 'gatsby';
 
 export const onRenderBody: GatsbySSR['onRenderBody'] = ({
-  setHeadComponents,
   setHtmlAttributes,
 }) => {
   setHtmlAttributes({
     lang: 'ja-JP',
   });
-
-  setHeadComponents([
-    // FIXME: extract to a plugin
-    <meta
-      key="color-scheme"
-      name="color-scheme"
-      content="light dark"
-    />,
-  ]);
 };
