615c82eda98c44a4c7a4790cbca0d8d0c3242cfe
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2022-12-13 17:12:52 +0900

    chore: update gatsby ssr (#771)

--- ca.karrotmarket.com/gatsby-ssr.tsx
+++ ca.karrotmarket.com/gatsby-ssr.tsx
@@ -1,26 +1,9 @@
-import * as React from 'react';
 import { type GatsbySSR } from 'gatsby';
 
 export const onRenderBody: GatsbySSR['onRenderBody'] = ({
-  setHeadComponents,
   setHtmlAttributes,
 }) => {
   setHtmlAttributes({
     lang: 'en-CA',
-
-    // FIXME: extract to a plugin
-    // @ts-ignore
-    'data-seed': 'light-only',
-    // @ts-ignore
-    'data-seed-scale-color': 'light',
   });
-
-  setHeadComponents([
-    // FIXME: extract to a plugin
-    <meta
-      key="color-scheme"
-      name="color-scheme"
-      content="light dark"
-    />,
-  ]);
 };
