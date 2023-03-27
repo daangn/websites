c008adc81bf62195390dcd3e5df8b4e6e06f3b82
Tony <jeehyukwon@gmail.com>
2023-03-27 11:46:57 +0900

    remove comment

--- _packages/@karrotmarket/gatsby-theme-brochure/src/components/prismicBrochureDataBodyHowToSection/SubtitleContainer.tsx
+++ _packages/@karrotmarket/gatsby-theme-brochure/src/components/prismicBrochureDataBodyHowToSection/SubtitleContainer.tsx
@@ -3,36 +3,6 @@ import { rem } from "polished";
 
 const SubtitleContainer = styled("span", {
   display: 'none',
-  // display: "inline-block",
-  // position: 'relative',
-  // fontWeight: 'bold',
-  // fontSize: rem(30),
-  // padding: "0 .75rem",
-  // marginBottom: rem(10),
-  // "::before": {
-  //   display: 'block',
-  //   content: "",
-  //   width: '4px',
-  //   height: '90%',
-  //   position: 'absolute',
-  //   top: 0,
-  //   left: 0,
-  //   backgroundColor: vars.$scale.color.carrot700,
-  //   borderRadius: '2px',
-  //   transform: 'rotate(-15deg)'
-  // },
-  // "::after": {
-  //   display: 'block',
-  //   content: "",
-  //   width: '4px',
-  //   height: '90%',
-  //   position: 'absolute',
-  //   top: 0,
-  //   right: 0,
-  //   backgroundColor: vars.$scale.color.carrot700,
-  //   borderRadius: '2px',
-  //   transform: 'rotate(15deg)'
-  // }
 })
 
 export default SubtitleContainer
