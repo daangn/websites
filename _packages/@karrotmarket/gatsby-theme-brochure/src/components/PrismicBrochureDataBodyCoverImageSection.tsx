8486e692201b465a7c42878d51dbc35b41666f45
Hyeseong Kim <tim@daangn.com>
2023-03-27 12:47:40 +0900

    jp.karrotmarket.com: service intro page (#898)
    * jp.karrotmarket.com: wip service intro page
    
    * WIP
    
    * markup
    
    * remove comment
    
    * format
    
    * fix
    
    ---------
    
    Co-authored-by: Tony <jeehyukwon@gmail.com>

---
+++ _packages/@karrotmarket/gatsby-theme-brochure/src/components/PrismicBrochureDataBodyCoverImageSection.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { styled } from 'gatsby-theme-stitches/src/config';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+
+import RootContainer from './prismicBrochureDataBodyCoverImageSection/RootContainer';
+
+export const fragments = graphql`
+  fragment PrismicBrochureDataBodyCoverImageSection_section on PrismicBrochureDataBodyCoverImageSection {
+    primary {
+      section_id
+
+      title {
+        html
+      }
+
+      cover_image {
+        alt
+        localFile {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        thumbnails {
+          wide {
+            alt
+            localFile {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+type Props = {
+  section: GatsbyTypes.PrismicBrochureDataBodyCoverImageSection_sectionFragment;
+};
+
+export default function PrismicBrochureDataBodyCoverImageSection({ section }: Props) {
+  const wideImage =
+    section.primary.cover_image?.thumbnails?.wide?.localFile?.childImageSharp?.gatsbyImageData;
+
+  return (
+    <RootContainer id={section.primary.section_id || undefined}>
+      <ImageContainer>
+        <GatsbyImage
+          image={wideImage}
+          alt={section.primary.cover_image.alt}
+          objectFit="cover"
+          style={{
+            minHeight: '18.75rem',
+          }}
+        />
+      </ImageContainer>
+    </RootContainer>
+  );
+}
+
+const ImageContainer = styled('div', {
+  objectFit: 'cover',
+  minHeight: '18.75rem',
+});
---
+++ _packages/@karrotmarket/gatsby-theme-brochure/src/components/prismicBrochureDataBodyHowToSection/SubtitleContainer.tsx
@@ -0,0 +1,8 @@
+import { styled } from 'gatsby-theme-stitches/src/config';
+import { rem } from 'polished';
+
+const SubtitleContainer = styled('span', {
+  display: 'none',
+});
+
+export default SubtitleContainer;
