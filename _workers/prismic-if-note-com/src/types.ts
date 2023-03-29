b42d834aa4069aedc30887e3f2a07aa1284752b5
ginger-kang <kdhoon07@gmail.com>
2023-03-30 02:40:43 +0900

    feat: note.com prismic integration field worker

---
+++ _workers/prismic-if-note-com/src/types.ts
@@ -0,0 +1,13 @@
+export type IntegrationFieldsFormat = {
+  results_size: number;
+  results: IntegrationFieldsItem[];
+};
+
+export type IntegrationFieldsItem = {
+  id: string;
+  title: string;
+  description: string;
+  image_url: string;
+  last_update: string;
+  blob: unknown;
+};
