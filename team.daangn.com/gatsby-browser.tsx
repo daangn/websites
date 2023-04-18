4fa4444621bda9faa53c529c90080348d765b721
Minji Choi <eunoia2040@gmail.com>
2023-04-18 15:36:26 +0900

    refactor: lint code style

--- team.daangn.com/gatsby-browser.tsx
+++ team.daangn.com/gatsby-browser.tsx
@@ -1,7 +1,5 @@
 export { wrapRootElement } from '@karrotmarket/gatsby-theme-prismic/src/wrapRootElement';
 
 export const onClientEntry = async () => {
-  window.location.replace(
-    `https://about.daangn.com${window.location.pathname}`
-  );
+  window.location.replace(`https://about.daangn.com${window.location.pathname}`);
 };
