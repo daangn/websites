2444bfe3f299e11cb253bf15d98004d1cc988309
Minji Choi <eunoia2040@gmail.com>
2023-04-18 21:11:25 +0900

    refactor: remove redirection logic

--- team.daangn.com/gatsby-browser.tsx
+++ team.daangn.com/gatsby-browser.tsx
@@ -1,5 +1,1 @@
 export { wrapRootElement } from '@karrotmarket/gatsby-theme-prismic/src/wrapRootElement';
-
-export const onClientEntry = async () => {
-  window.location.replace(`https://about.daangn.com${window.location.pathname}`);
-};
