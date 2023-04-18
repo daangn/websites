204d2eb4d73885b312343afdc7251963d65f1397
Minji Choi <eunoia2040@gmail.com>
2023-04-18 15:24:07 +0900

    feat: add redirection for canonical url

--- team.daangn.com/gatsby-browser.tsx
+++ team.daangn.com/gatsby-browser.tsx
@@ -1,1 +1,7 @@
 export { wrapRootElement } from '@karrotmarket/gatsby-theme-prismic/src/wrapRootElement';
+
+export const onClientEntry = async () => {
+  window.location.replace(
+    `https://about.daangn.com${window.location.pathname}`
+  );
+};
