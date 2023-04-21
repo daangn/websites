0f561038d35a3ad4131bbabcb6e4b25ced18bd11
Minji Choi <eunoia2040@gmail.com>
2023-04-21 12:07:44 +0900

    update: add ignore case

--- _packages/@karrotmarket/gatsby-theme-website-team/src/utils/common.ts
+++ _packages/@karrotmarket/gatsby-theme-website-team/src/utils/common.ts
@@ -3,7 +3,13 @@ export function lookup<T = unknown>(key: string, source: Record<string, T>) {
 }
 
 export function isCanonicalUrl(url: string): boolean {
-  const canonical = 'https://about.daangn.com/';
+  const ignore = 'karrotmarket.com';
+
+  if (url.includes(ignore)) {
+    return true;
+  }
+
+  const canonical = 'about.daangn.com';
 
   if (url.includes(canonical)) {
     return true;
