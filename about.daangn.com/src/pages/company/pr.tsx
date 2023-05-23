2015cf723357dc5dae2d73ae869595346317f45e
Minji Choi <eunoia2040@gmail.com>
2023-05-23 22:00:23 +0900

    refactor: update pr page

--- about.daangn.com/src/pages/company/pr.tsx
+++ about.daangn.com/src/pages/company/pr.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { graphql, type PageProps } from 'gatsby';
 import { styled } from 'gatsby-theme-stitches/src/config';
 import PrCard from '../../components/pr/PrCard';
+import { rem } from 'polished';
 
 type PrPageProps = PageProps<GatsbyTypes.PrPageQuery>;
 
@@ -18,26 +19,40 @@ export const query = graphql`
 `;
 
 const PrPage: React.FC<PrPageProps> = ({ data }) => {
+  console.log('data ::', data);
   return (
-    <Container>
-      {data.allPost.nodes.map((node) => (
-        <PrCard key={node.slug} data={node} />
-      ))}
-    </Container>
+    <Main>
+      <Title>당근마켓 보도자료</Title>
+      {data.allPost.nodes.length === 0 ? (
+        <NoPostDescription>등록된 글이 없습니다.</NoPostDescription>
+      ) : (
+        <section>
+          {data.allPost.nodes.map((node) => (
+            <PrCard key={node.slug} data={node} />
+          ))}
+        </section>
+      )}
+    </Main>
   );
 };
 
-export const Head: React.FC = () => {
-  return (
-    <>
-      <meta name="robots" content="noindex" />
-    </>
-  );
-};
-
-const Container = styled('main', {
+const Main = styled('main', {
   contentArea: true,
   contentSpaceTop: true,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+});
+
+const Title = styled('h1', {
+  marginBottom: rem(80),
+});
+
+const NoPostDescription = styled('div', {
+  width: '100%',
+  marginTop: rem(64),
+  textAlign: 'center',
 });
 
 export default PrPage;
