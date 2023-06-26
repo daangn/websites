38bea482868846a0872dcfe440c50c58d8c422cf
carsonmj <eunoia2040@gmail.com>
2023-06-26 17:25:43 +0900

    feat: add type for svg

---
+++ about.daangn.com/src/types/svgr.d.ts
@@ -0,0 +1,10 @@
+declare module '*.svg' {
+  import type * as React from 'react';
+
+  interface SvgrComponent extends React.FunctionComponent<React.SVGAttributes<SVGElement>> {}
+
+  const url: string;
+  export default url;
+
+  export const ReactComponent: SvgrComponent;
+}
