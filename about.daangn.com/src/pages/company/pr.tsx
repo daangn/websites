1e48182c9b9f6a51e29c2c954455c2478b469982
Minji Choi <eunoia2040@gmail.com>
2023-06-28 22:27:14 +0900

    feat: add meta tags

--- about.daangn.com/src/pages/company/pr.tsx
+++ about.daangn.com/src/pages/company/pr.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { graphql, type PageProps } from 'gatsby';
+import { graphql, type PageProps, type HeadProps, } from 'gatsby';
+import { HeadSeo, OpenGraph, TwitterCard } from 'gatsby-plugin-head-seo/src';
 import { styled } from 'gatsby-theme-stitches/src/config';
 import { rem } from 'polished';
 
@@ -10,6 +11,20 @@ type PrPageProps = PageProps<GatsbyTypes.PrPageQuery>;
 export const query = graphql`
   query PrPage($locale: String!, $navigationId: String!) {
     ...TeamWebsite_DefaultLayout_query
+
+    prismicTeamContents(
+      lang: { eq: $locale }
+    ) {
+      data {
+        main_page_meta_image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
 
     allPost(
       filter: {category: {name: {eq: "pr"}}}
@@ -60,10 +75,47 @@ const NoPostDescription = styled('div', {
 
 export default PrPage;
 
-export const Head: React.FC = () => {
+type PrPageHeadProps = HeadProps<GatsbyTypes.PrPageQuery>;
+
+export const Head: React.FC<PrPageHeadProps> = ({ data, location }) => {
+  const metaTitle = '당근마켓 보도자료';
+  const metaDescription = '당근마켓에서 제공하는 공식 보도자료입니다.';
+  /**
+   * (임시)어바웃당근 메인페이지와 동일한 og 이미지 사용
+   */
+  const metaImage =
+    data.prismicTeamContents.data.main_page_meta_image?.localFile?.childImageSharp?.fixed;
+
   return (
-    <>
-      <meta name="robots" content="noindex, nofollow" />
-    </>
+    <HeadSeo location={location} title={metaTitle} description={metaDescription}>
+      {(props) => [
+        <OpenGraph
+          key='og'
+          og={{
+            ...props,
+            type: 'website',
+            ...(metaImage && {
+              images: [
+                {
+                  url: new URL(
+                    metaImage.src,
+                    metaImage.src.startsWith('http') ? metaImage.src : props.url,
+                  ),
+                  width: metaImage.width,
+                  height: metaImage.height,
+                },
+              ],
+            }),
+          }}
+        />,
+        <TwitterCard
+          key='twitter'
+          card={{
+            ...props,
+            type: 'summary_large_image',
+          }}
+        />,
+      ]}
+    </HeadSeo>
   );
 };
