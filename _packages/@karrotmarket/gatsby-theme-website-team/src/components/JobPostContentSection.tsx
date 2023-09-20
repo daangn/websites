2118f963b2cafae7eae6665cebc95fc877fd286d
MINJI <54696956+carsonmj@users.noreply.github.com>
2023-09-20 18:44:19 +0900

    style: remove gap (#994)

--- _packages/@karrotmarket/gatsby-theme-website-team/src/components/JobPostContentSection.tsx
+++ _packages/@karrotmarket/gatsby-theme-website-team/src/components/JobPostContentSection.tsx
@@ -17,7 +17,6 @@ export const query = graphql`
 const Container = styled('section', {
   display: 'flex',
   flexDirection: 'column',
-  gap: '1.5rem',
   '&:not(:last-child)': {
     marginBottom: rem(56),
   },
