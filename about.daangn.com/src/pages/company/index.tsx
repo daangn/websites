873dcceb09ae38cc650a5af471bbaf3f5c9146fc
Tony Won <tony@daangn.com>
2025-07-16 11:02:19 +0900

    format

--- about.daangn.com/src/pages/company/index.tsx
+++ about.daangn.com/src/pages/company/index.tsx
@@ -132,7 +132,10 @@ export const Head: React.FC<CompanyPageHeadProps> = ({ data, location }) => {
             type: 'summary_large_image',
           }}
         />,
-        <link rel="stylesheet" href="https://assets.krrt.io/daangn/branding-assets/0.0.2/typography/fonts/KarrotSans.css" />
+        <link
+          rel="stylesheet"
+          href="https://assets.krrt.io/daangn/branding-assets/0.0.2/typography/fonts/KarrotSans.css"
+        />,
       ]}
     </HeadSeo>
   );
