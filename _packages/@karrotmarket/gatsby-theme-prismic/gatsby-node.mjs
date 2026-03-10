33f231325f670df598859e916a4e3164daa134bc
Hyeseong Kim <tim@daangn.com>
2026-03-11 00:13:42 +0900

    hacky enforcement to use proxy endpoint

--- _packages/@karrotmarket/gatsby-theme-prismic/gatsby-node.mjs
+++ _packages/@karrotmarket/gatsby-theme-prismic/gatsby-node.mjs
@@ -13,25 +13,18 @@ export const createResolvers = ({ createResolvers }) => {
   createResolvers({
     PrismicImageField: {
       url: {
-				type: "String",
-				description: "The image's URL.",
-				args: {
-					imgixParams: {
-						type: "PrismicImgixURLParams",
-						description:
-							"Parameters to modify the image output using Imgix's URL API. To learn more, see: https://docs.imgix.com/apis/rendering",
-					},
-				},
 				resolve: async (source, args, ctx, info) => {
           const url = await info.originalResolver(source, args, ctx, info);
           return replaceImageHost(url);
 				},
       },
       gatsbyImageData: {
-        type: "JSON",
         resolve: async (source, args, ctx, info) => {
-          const image = await info.originalResolver(source, args, ctx, info);
-          return getCdnImage(image);
+          if (!source.url) return null;
+
+          const copy = { ...source, url: replaceImageHost(source.url) };
+          const image = await info.originalResolver(copy, args, ctx, info);
+          return image && getCdnImage(image);
         },
       },
     },
